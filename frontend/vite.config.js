82f40ca4080abac96597e422d797273d11e9e94f
Sakshi Pandey <127128155+sakshi95690@users.noreply.github.com>
2024-11-16 10:22:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,9 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-   build: {
-    rollupOptions: {
-      external: ['@radix-ui/react-slot'], // Add this line
-    },
-  },
+  
 })
