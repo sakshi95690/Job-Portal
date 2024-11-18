2518ac556507a89760c90a9493c4a57200fffb23
Sakshi Pandey <127128155+sakshi95690@users.noreply.github.com>
2024-11-18 10:51:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:8000',
+    origin:'https://job-portal-1-iiot.onrender.com',
     credentials:true
 }
 
